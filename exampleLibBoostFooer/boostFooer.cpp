ea7f73c185e30b8e1976a446a96bdee68cc2e459
Simone Gasparini <simone.gasparini@gmail.com>
2017-03-09 11:37:14 +0100

    [example] added example using libBoostFooer

---
+++ exampleLibBoostFooer/boostFooer.cpp
@@ -0,0 +1,11 @@
+#include <boostFooer/BoostFooer.hpp>
+#include <cstdlib> // EXIT_SUCCESS
+#include <iostream> // std::cout
+
+int main() {
+  std::cout << "Hello, exampleBoostFooer!" << std::endl;
+  boostFooer::BoostFooer::say();
+  return EXIT_SUCCESS;
+};
+
+
