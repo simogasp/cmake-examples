86adc2e7a6932ab6c2ebc07c1f25bc7dc08a96d1
yhnu <buutuud@gmail.com>
2021-04-27 14:46:34 +0800

    clean code

--- exampleLibFoo/boo.cpp
+++
@@ -1,9 +0,0 @@
-#include <foo/Bar.hpp>
-#include <cstdlib> // EXIT_SUCCESS
-#include <iostream> // std::cout
-
-int main() {
-  std::cout << "Hello, Boo!" << std::endl;
-  foo::Bar::say();
-  return EXIT_SUCCESS;
-}
--- exampleLibFooer/booer.cpp
+++
@@ -1,9 +0,0 @@
-#include <fooer/Fooer.hpp>
-#include <cstdlib> // EXIT_SUCCESS
-#include <iostream> // std::cout
-
-int main() {
-  std::cout << "Hello, Booer!" << std::endl;
-  fooer::Fooer::say();
-  return EXIT_SUCCESS;
-}
